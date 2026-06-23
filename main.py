ab1d4e0afd95ad0f7cc393d40829ca9568d91112
TunDrNapisz <naffizsazali02@gmail.com>
2026-06-23 20:52:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,10 +45,11 @@ executor = ThreadPoolExecutor(max_workers=3)
 
 
 
-@app.before_request
-def set_backend_url():
-    global BACKEND_URL
-    BACKEND_URL = request.host_url.rstrip('/')
+# # PADAM ATAU COMMENT BAHAGIAN INI
+# @app.before_request
+# def set_backend_url():
+#     global BACKEND_URL
+#     BACKEND_URL = request.host_url.rstrip('/')
 
 # Beritahu python untuk baca 'groq.env'
 load_dotenv(dotenv_path='groq.env')
